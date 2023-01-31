563361f76e370e5440d4d0435b862efb495c8f2b
Parfen774 <113260962+Parfen774@users.noreply.github.com>
2023-01-31 18:57:42 +0500

    Added CSV files

--- Code/Book.cs
+++ Code/Book.cs
@@ -7,14 +7,16 @@ namespace Dummy_Database
         public string Author { get; set; }
         public string Name { get; set; }
         public DateTime Year { get; set; }
+        public bool IsUsed { get; set; }
         public int Wardrobe { get; set; }
         public int Shelf { get; set; }
 
-        public Book(string author, string name, string year, int wardrobe, int shelf)
+        public Book(string author, string name, string year, bool isUsed, int wardrobe, int shelf)
         {
             Author = author;
             Name = name;
             Year = Convert.ToDateTime(year);
+            IsUsed = isUsed;
             Wardrobe = wardrobe;
             Shelf = shelf;
         }
